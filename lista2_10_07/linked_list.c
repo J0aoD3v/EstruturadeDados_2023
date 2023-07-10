7aa3e4e71a814b3f0b3357692678867696e5341f
João Cláudio <jcmichelato8@gmail.com>
2023-07-10 16:34:04 -0300

    .

---
+++ lista2_10_07/linked_list.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+// Função para criar um novo nó
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Erro: Falha na alocação de memória.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Função para inserir um nó no início da lista
+void insertNode(Node** head, int data) {
+    Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Função para remover um nó da lista
+void removeNode(Node** head, int data) {
+    Node* temp = *head;
+    Node* prev = NULL;
+
+    if (temp != NULL && temp->data == data) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != data) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Erro: O elemento %d não está na lista.\n", data);
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+// Função para imprimir os elementos da lista
+void printList(Node* head) {
+    Node* temp = head;
+    printf("Lista: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Função para liberar a memória alocada pela lista
+void freeList(Node* head) {
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
---
+++ lista2_10_07/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "linked_list.h"
+
+int main() {
+    Node* lista = NULL;
+
+    // Inserindo elementos na lista
+    insertNode(&lista, 10);
+    insertNode(&lista, 20);
+    insertNode(&lista, 30);
+    insertNode(&lista, 40);
+
+    // Imprimindo a lista
+    printList(lista);
+
+    // Removendo um elemento da lista
+    removeNode(&lista, 20);
+
+    // Imprimindo a lista novamente
+    printList(lista);
+
+    // Liberando a memória alocada pela lista
+    freeList(lista);
+
+    return 0;
+}
